de4231bb780a3b52f8369a5d667c30388d8492e5
Didi Prasetyo <didiprasetyo36@gmail.com>
2023-12-04 13:17:10 +0700

    create mqtt client

---
+++ app/mqtt.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"fmt"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, message mqtt.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", message.Payload(), message.Topic())
+}
+
+var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
+	fmt.Println("Connected!")
+}
+
+var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
+	fmt.Println("Connect lost: %v", err)
+}
+
+func NewMqttClient() mqtt.Client {
+	broker := "broker.emqx.io"
+	port := 1883
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(fmt.Sprintf("tcp://%s:%d", broker, port))
+	opts.SetClientID("go_mqtt_pub")
+	opts.SetUsername("emqx")
+	opts.SetPassword("public")
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	return client
+}
+
