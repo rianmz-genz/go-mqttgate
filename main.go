ec4d4985d39873fbc3c1e2249a5b2892aa4329c9
Didi Prasetyo <didiprasetyo36@gmail.com>
2023-12-04 13:17:41 +0700

    add example publish message

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"adriandidimqttgate/app"
+	"fmt"
+)
+
 
 func main() {
+	fmt.Println("Hello!")
+
+	token := app.NewMqttClient().Publish("testtopic/esp8266", 0, false, "Hello from GO!")
+	token.Wait()
 }
