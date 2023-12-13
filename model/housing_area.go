f651388fac32dc46125b02be567795a5b9877c86
Didi Prasetyo <didiprasetyo36@gmail.com>
2023-12-13 10:59:32 +0700

    refactor file directory

--- model/housing_area.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type HousingArea struct {
-    ID      int64  `gorm:"primaryKey" json:"id"`
-    Name    string `gorm:"type:varchar(100)" json:"name"`
-    Code    string `gorm:"type:varchar(6)" json:"code"`
-    Address string `gorm:"type:varchar(300)" json:"address"`
-    Users   []User `gorm:"foreignKey:HousingAreaID"`
-}
