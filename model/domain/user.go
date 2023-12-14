ec8575346c41c391d008a1aaeb5fa733019298b3
Didi Prasetyo <didiprasetyo36@gmail.com>
2023-12-15 01:23:54 +0700

    update user model

--- model/domain/user.go
+++ model/domain/user.go
@@ -4,9 +4,10 @@ import "gorm.io/gorm"
 
 type User struct {
 	gorm.Model
+	Name     string `gorm:"type:varchar(100)" json:"name"`
 	Email    string `gorm:"type:varchar(100)" json:"email"`
 	Password string `json:"-"`
-	OfficeID int64  `json:"officeId"`
+	OfficeID uint   `json:"officeId"`
 	Office   Office
 	Sessions []Session
 }
