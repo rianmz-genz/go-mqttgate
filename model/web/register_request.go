425e5d680a8d955d52e05eb0af81f783da60cc5a
Didi Prasetyo <didiprasetyo36@gmail.com>
2023-12-15 00:28:12 +0700

    web request

---
+++ model/web/register_request.go
@@ -0,0 +1,7 @@
+package web
+
+type RegisterRequest struct {
+	Name     string `validate:"required,min=1,max=250" json:"name"`
+	Email    string `validate:"required,min=1,max=250" json:"email"`
+	Password string `validate:"required,min=1,max=250" json:"password"`
+}
