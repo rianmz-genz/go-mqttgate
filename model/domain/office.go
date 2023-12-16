8b26cd2bbbdc07f8c19549b42a559b64c0fcd72e
Didi Prasetyo <didiprasetyo36@gmail.com>
2023-12-16 11:25:53 +0700

    add unique

--- model/domain/office.go
+++ model/domain/office.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Office struct {
 	gorm.Model
 	Name    string `gorm:"type:varchar(100)" json:"name"`
-	Code    string `gorm:"type:varchar(6),index:,unique" json:"code"`
+	Code    string `gorm:"type:varchar(6);index:;unique" json:"code"`
 	Address string `gorm:"type:varchar(300)" json:"address"`
 	Users   []User
 }
