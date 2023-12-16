e7add723961d32c06a8fa63e0ca848ea77a37aa3
Didi Prasetyo <didiprasetyo36@gmail.com>
2023-12-16 11:07:22 +0700

    code is unique

--- model/domain/office.go
+++ model/domain/office.go
@@ -3,9 +3,9 @@ package domain
 import "gorm.io/gorm"
 
 type Office struct {
-    gorm.Model
-    Name    string `gorm:"type:varchar(100)" json:"name"`
-    Code    string `gorm:"type:varchar(6)" json:"code"`
-    Address string `gorm:"type:varchar(300)" json:"address"`
-    Users   []User
+	gorm.Model
+	Name    string `gorm:"type:varchar(100)" json:"name"`
+	Code    string `gorm:"type:varchar(6),index:,unique" json:"code"`
+	Address string `gorm:"type:varchar(300)" json:"address"`
+	Users   []User
 }
