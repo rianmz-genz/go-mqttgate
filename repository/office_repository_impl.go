acac54972551132f595925d3f243a524d29e432d
Didi Prasetyo <didiprasetyo36@gmail.com>
2023-12-26 16:39:28 +0700

    add function save

--- repository/office_repository_impl.go
+++ repository/office_repository_impl.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"adriandidimqttgate/helper"
 	"adriandidimqttgate/model/domain"
 
 	"golang.org/x/net/context"
@@ -17,6 +18,16 @@ func NewOfficeRepository() OfficeRepository {
 func (repository *OfficeRepositoryImpl) FindOfficeByCode(ctx context.Context, db *gorm.DB, code string) domain.Office {
 	office := domain.Office{}
 	db.WithContext(ctx).Where("id = ?", code).Preload("").First(office)
+
+	return office
+}
+
+func (repository *OfficeRepositoryImpl) Save(ctx context.Context, db *gorm.DB, office domain.Office) domain.Office {
+	result := db.WithContext(ctx).Create(&office)
+
+	if result.Error != nil {
+		helper.PanicIfError(result.Error)
+	}
 
 	return office
 }
