fb226ded3eb89427c30b315fc9b4698f059a5785
Didi Prasetyo <didiprasetyo36@gmail.com>
2023-12-26 14:40:58 +0700

    function GetUsersByOfficeId

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -8,6 +8,7 @@ import (
 )
 
 type UserRepository interface {
+	GetUsersByOfficeId(ctx context.Context, db *gorm.DB, officeId uint) ([]domain.User, error)
 	GetUserByEmail(ctx context.Context, db *gorm.DB, user domain.User) (domain.User, error)
 	Save(ctx context.Context, db *gorm.DB, user domain.User) domain.User
 	GetUserById(ctx context.Context, db *gorm.DB, userId uint) domain.User
--- repository/user_repository_impl.go
+++ repository/user_repository_impl.go
@@ -14,6 +14,17 @@ func NewUserRepository() UserRepository {
 	return &UserRepositoryImpl{}
 }
 
+func (repository UserRepositoryImpl) GetUsersByOfficeId(ctx context.Context, db *gorm.DB, officeId uint) ([]domain.User, error) {
+	var users []domain.User
+	result := db.WithContext(ctx).Where("office_id = ?", officeId).Find(&users)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return users, nil
+}
+
 func (repository UserRepositoryImpl) GetUserByEmail(ctx context.Context, db *gorm.DB, user domain.User) (domain.User, error) {
 	result := db.WithContext(ctx).Where("email = ?", user.Email).First(&user)
 
